0791ca6815bf81d3f4cb8951577777aaa14dd7d6
Marc Boanas <marcjohnboanas@gmail.com>
2013-01-29 14:22:35 +0000

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 AccountsHero::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
