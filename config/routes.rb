a5d01ba26d7fd04bd6c8f671b50bef0b57f4d527
Marc Boanas <marcjohnboanas@gmail.com>
2013-02-03 20:30:14 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 AccountsHero::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  root :to => 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/signup',    to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,8 +53,6 @@ AccountsHero::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
